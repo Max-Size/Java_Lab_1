fb1f57e47e1c029d144ffe7390394c6cba04ae2b
Maxim Seleznev <125485047+Max-Size@users.noreply.github.com>
2023-03-09 00:07:18 +0300

    Add files via upload
    It is a program that count amount of each symbol from file and write it down in  another file

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args){
+        Statistics statistics = new Statistics();
+        statistics.makeStatFromFile();
+        statistics.writeStatToFile();
+    }
+}
